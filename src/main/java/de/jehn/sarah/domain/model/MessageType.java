3a019f4439d018251036bc89da1af9780c56f5cf
Sarah Jehn <jehn.sarah@gmail.com>
2016-04-18 23:07:25 +0200

    added missing enum

--- src/main/java/de/jehn/sarah/domain/model/MessageType.java
+++ src/main/java/de/jehn/sarah/domain/model/MessageType.java
@@ -4,5 +4,5 @@ package de.jehn.sarah.domain.model;
  * Created by sarahjehn on 19.03.16.
  */
 public enum MessageType {
-    NO_MATCH, NEW_GOAL, GREETING, RETRIEVE_RECENT_GOALS, REMOVE_GOAL, NON_RELEVANT
+    NO_MATCH, NEW_GOAL, GREETING, RETRIEVE_RECENT_GOALS, REMOVE_GOAL, NON_RELEVANT, BYE_BYE
 }
